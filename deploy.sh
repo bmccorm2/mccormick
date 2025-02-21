4f34feb0ddd0f263493132c58a0853d1d0cc83c0
Bryan McCormick <bryan@mccormick.sh>
2025-02-21 09:00:29 -0700

    upgraded to pihole v6

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync -trhPuL --exclude-from=deployExclude.txt --delete docker mccormick:/opt
+rsync -trhPuL --exclude-from=deployExclude.txt --delete docker mccormick.sh:/opt
 
 # Service must be built on host machine because arch is different
 # rsync -trhPuLv --exclude-from=deployExclude.txt --exclude service/node_modules --delete service 1mccormick:/opt/temp
